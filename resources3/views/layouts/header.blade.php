9b21b1da2a97068965570d7adbd43b30019141eb
krsdata <krsdata.net@gmail.com>
2017-04-29 04:30:00 +0530

    g

---
+++ resources3/views/layouts/header.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- Meta -->
+<meta chaRSet="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="keywords" content="MediaCenter, Template, eCommerce">
+<meta name="robots" content="all">
+<title> 
+    {{ isset($website_title->field_value)?$website_title->field_value:"GuruHomeShop:  India Largest Kirana store" }} 
+</title>
+
+<!-- Bootstrap Core CSS -->
+<link rel="stylesheet" href="{{ asset('public/enduser/assets/css/bootstrap.min.css') }}">
+
+<!-- Customizable CSS -->
+<link rel="stylesheet" href="{{ asset('public/enduser/assets/css/main.css') }}">
+<link rel="stylesheet" href="{{ asset('public/enduser/assets/css/blue.css') }}">
+<link rel="stylesheet" href="{{ asset('public/enduser/assets/css/owl.carousel.css') }}">
+<link rel="stylesheet" href="{{ asset('public/enduser/assets/css/owl.transitions.css') }}">
+<link rel="stylesheet" href="{{ asset('public/enduser/assets/css/animate.min.css') }}">
+<link rel="stylesheet" href="{{ asset('public/enduser/assets/css/rateit.css') }}">
+<link rel="stylesheet" href="{{ asset('public/enduser/assets/css/bootstrap-select.min.css') }}">
+
+<!-- Icons/Glyphs -->
+<link rel="stylesheet" href="{{ asset('public/enduser/assets/css/font-awesome.css')}}">
+
+ 
+
+</style>
+</head>
+<body class="cnt-home"> 
+ 
